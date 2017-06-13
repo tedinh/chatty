553e79288d4d218adfe9a59f72659e11bc926390
tedinh <tedinh@gmail.com>
2017-06-13 16:27:08 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+var messages = [];
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+messages.push({ message: req.body.message, time: new Date() });
+res.status(200).json({ messages: messages });
+});
+
+
+
+app.listen(3000, function(){
+  console.log('Example app listening on port 3000');
+});
